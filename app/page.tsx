a9b7df707063e6c1e7e4c7d6951337a80fa26f65
yuvraj saini <mr.yuvrajsaini@gmail.com>
2025-05-15 12:16:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/dashboard");
-  // return <div>nishant jangid</div>;
+  // return <div>yuvraj saini</div>;
 }
